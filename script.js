b83437ed631180ab3554114f8bfb512399f4544f
Barani1912 <baranivignesh0024@gmail.com>
2024-10-31 09:15:45 +0530

    confetti

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['🔥', '⚡️', '💥', '✨'],
+    }).then(() => jsConfetti.addConfetti())
+})
